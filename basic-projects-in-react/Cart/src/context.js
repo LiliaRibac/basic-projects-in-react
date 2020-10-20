c94645f38c5c6c89b522b528b13f790978df8f3c
LiliaRibac <ribac.lilia@gmai.com>
2020-10-20 13:51:12 -0700

    fetch data && refactoring the code

--- basic-projects-in-react/Cart/src/context.js
+++ basic-projects-in-react/Cart/src/context.js
@@ -38,6 +38,23 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'GET_TOTALS' });
   }, [state.cart]);
 
+  // fetch data
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart });
+  };
+
+  // refactoring the code in reducer.js
+  const toggleAmount = (id, type) => {
+    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
@@ -46,6 +63,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
       }}
     >
       {children}
